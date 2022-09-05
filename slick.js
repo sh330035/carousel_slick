6b8dcb777a7803a5e20408a221d98f7c9728751d
david3335 <sh330035@gmail.com>
2022-09-05 13:58:16 +0800

    update slick refresh

--- slick.js
+++ slick.js
@@ -1,5 +1,19 @@
+// 頁籤切換控制
+$(document).ready(function() {
+  $(".test-tabs a").click(function(event) {
+      event.preventDefault();
+      $(this).parent().addClass("current");
+      $(this).parent().siblings().removeClass("current");
+      let tab = $(this).attr("href");
+      $(".tab-content").not(tab).css("display", "none");
+      $(tab).css("display", "flex");
+      $('.test-video').slick("refresh");
+      $('.test-pic').slick("refresh");
+      $('.test-art').slick("refresh");
+  });
+});
 
-
+// 輪播設定
 $('.test-video').slick({
   infinite: false,
   slidesToShow: 1,
